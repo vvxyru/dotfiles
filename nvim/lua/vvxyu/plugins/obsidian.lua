fc583918e875f34b6ca1bed37c9dad6646428d8a
vvxyu <vincentgabrielumali@gmail.com>
2024-11-09 14:37:30 -0700

    nv: configured obsidian plugin

--- nvim/lua/vvxyu/plugins/obsidian.lua
+++ nvim/lua/vvxyu/plugins/obsidian.lua
@@ -1,12 +1,17 @@
 return {
     "epwalsh/obsidian.nvim",
-    version = "*",
+    version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
+    event = {
+        "BufReadPre /home/vvxyu/docs/notes/*.md",
+        "BufNewFile /home/vvxyu/docs/notes/*.md",
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
     config = function()
+        local map = vim.keymap.set
+
         local function toggle_conceal()
             ---@diagnostic disable-next-line: undefined-field
             if vim.opt.conceallevel:get() == 2 then
@@ -16,7 +21,7 @@ return {
             end
         end
 
-        vim.keymap.set("n", "<leader>eo", toggle_conceal)
+        map("n", "<leader>eo", toggle_conceal)
 
         vim.opt.conceallevel = 2
 
@@ -24,36 +29,48 @@ return {
             workspaces = {
                 {
                     name = "notes",
-                    path = "/home/vvxyu/docs/notes",
+                    path = "~/docs/notes",
+                    overrides = {
+                        notes_subdir = "root"
+                    },
                 },
             },
             completion = {
                 nvim_cmp = true,
             },
-            mappings = {
-                -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-                ["gf"] = {
-                    action = function()
-                        return require("obsidian").util.gf_passthrough()
-                    end,
-                    opts = { noremap = false, expr = true, buffer = true },
-                },
-                -- Toggle check-boxes.
-                ["<leader>ch"] = {
-                    action = function()
-                        return require("obsidian").util.toggle_checkbox()
-                    end,
-                    opts = { buffer = true },
-                },
-                -- Smart action depending on context, either follow link or toggle checkbox.
-                ["<cr>"] = {
-                    action = function()
-                        return require("obsidian").util.smart_action()
-                    end,
-                    opts = { buffer = true, expr = true },
-                },
-            },
-            new_notes_location = "/home/vvxyu/docs/notes/root",
+
+            new_notes_location = "notes_subdir",
+
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                local suffix = ""
+                if title ~= nil then
+                    suffix = title
+                else
+                    for _ = 1, 4 do
+                        suffix = suffix
+                    end
+                end
+                return suffix
+            end,
+
+            ---@return table
+            note_frontmatter_func = function(note)
+                if note.title then
+                    note:add_alias(note.title)
+                end
+
+                local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+                if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                    for k, v in pairs(note.metadata) do
+                        out[k] = v
+                    end
+                end
+
+                return out
+            end,
         })
-    end,
+    end
 }
